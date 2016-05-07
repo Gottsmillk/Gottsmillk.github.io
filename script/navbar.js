162dce226a8bd8c9cf12cfea311e891aea7da9ae
Gottsmillk <daniellee0217@yahoo.com>
2016-05-07 18:22:29 -0400

    v1.1.0.2
    - Attempt #2 at detecting if Chromium Wheel Scroller extension is
    enabled for chrome and to prompt
    the user to add it to the blacklist if it is detected.

--- script/navbar.js
+++ script/navbar.js
@@ -20,7 +20,6 @@ $(window).scroll(function() {
 });
 
 // Check if user has chromium scroll installed
-$(function() {
 var detect = function(base, if_installed, if_not_installed) {
     var s = document.createElement('script');
     s.onerror = if_not_installed;
@@ -30,7 +29,6 @@ var detect = function(base, if_installed, if_not_installed) {
 }
 detect('chrome-extension://' + khpcanbeojalbkpgpmjpdkjnkfcgfkhb, function() {alert('You seem to have chromium scroll wheel enabled. Please add this site to the blacklist since it does not support this extension.');});
 // use cookies to make sure that this is appears only once
-});
 
 // Smooth scrolling on scroll wheel
 $(function(){	
