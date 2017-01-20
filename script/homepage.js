761b1a72f44e1886b7bda695348f60c0130c3c81
Gottsmillk <daniellee0217@yahoo.com>
2017-01-20 14:06:28 -0500

    1-20-17
    on the road again~

--- script/homepage.js
+++ script/homepage.js
@@ -1,33 +1,39 @@
+var toggled = false;
 // Navbar static at top after scroll, fixes navbar size after scroll, fixes body positioning after scroll
 $(window).scroll(function() {
   if ($(document).scrollTop() < 80) {
 	$('.nav').css("position", "absolute");
 	$('.nav').css("top", "80px");
-  } else {
+	$('.sidebar').css("position", "absolute");
+	$('.sidebar').css("top", "120px");
+	} else {
 	$('.nav').css("position", "fixed");
-	$('.nav').css("top", "0px");	
+	$('.nav').css("top", "0px");
+	$('.sidebar').css("position", "fixed");	
+	$('.sidebar').css("top", "40px");
   }
 });
 
 //Sidebar opening and collapsing
 function sidebar() {
-	$('.sidebar').toggleClass('sidebarexpand');
+	if (toggled == false) {
+		$('.sidebar').animate({width: "160px"}, "fast");
+		toggled = true;
+	}
+	else if (toggled == true) {
+		$('.sidebar').animate({width: "0px"}, "fast");
+		toggled = false;
+	}
 }
 
 // Close the sidebar menu if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.link-sidebar a') 
   && !event.target.matches('.sidebar')
-  && !event.target.matches('.sidebar-header a')) {
-
-    var dropdowns = document.getElementsByClassName("sidebar");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('sidebarexpand')) {
-        openDropdown.classList.remove('sidebarexpand');
-      }
-    }
+  && !event.target.matches('.sidebar-header a')
+  && toggled == true) {
+	$('.sidebar').animate({width: "0px"}, "fast");
+	toggled = false;
   }
 }
 
@@ -112,3 +118,26 @@ function checkKeyPressed(e) {
 		}
     }
 }
+//Handling various aspect ratios
+function updateContainer() {
+	var w = $(window).width();
+	var h = $(window).height();
+    var r = w/h;
+	var x = 0;
+		if (w <= 1024) {
+			$('.pic1').css("width", "1024px");	
+			x = 1024;
+			$('.pic1').css("right", (x-(w+17))/2);
+		}
+		else {
+			$('.pic1').css("width", "100%");
+			$('.pic1').css("right", "0px");			
+		}
+	document.getElementById("test").textContent=w;
+}
+$(document).ready(function () {
+    updateContainer();
+    $(window).resize(function() {
+        updateContainer();
+    });
+});
